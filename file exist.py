5efcd4fdf6f4f84aab85e7e34d8bcd60b762adc4
Lassi Chen <lachen@utu.fi>
2023-05-31 06:41:55 +0300

    setup now uses the model after generating it

--- file exist.py
+++
@@ -1,8 +0,0 @@
-import os
-
-file_path = "model.h5"
-
-if os.path.exists(file_path):
-    print("File exists.")
-else:
-    print("File does not exist.")
--- setup.py
+++ setup.py
@@ -3,19 +3,14 @@ import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
+import os
 
 # Fetch the data using mnist
-train_images = mnist.train_images()
-train_labels = mnist.train_labels()
 test_images = mnist.test_images()
 test_labels = mnist.test_labels()
-
 # Normalize the images
-train_images = (train_images / 255) - 0.5
 test_images = (test_images / 255) - 0.5
-
 # Flatten the images
-train_images = train_images.reshape((-1, 784))
 test_images = test_images.reshape((-1, 784))
 
 # Build the model
@@ -25,35 +20,46 @@ model = Sequential([
     Dense(10, activation='softmax', ),
 ])
 
-# Configure the model
-model.compile(
-  optimizer='adam',
-  loss='categorical_crossentropy',
-  metrics=['accuracy'],
-)
+# if model.h5 exist, use it, if not generate one
+if os.path.exists("model.h5") == False:
+    # Train data and configuration
+    train_images = mnist.train_images()
+    train_labels = mnist.train_labels()
+    train_images = (train_images / 255) - 0.5
+    train_images = train_images.reshape((-1, 784))
 
-# Train the model
-model.fit(
-    train_images, # training data
-    to_categorical(train_labels), # training targets
-    epochs=5,
-    batch_size=32,
-)
+    # Configure the model
+    model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy'],
+    )
 
-# Test the model
-model.evaluate(
-    test_images,
-    to_categorical(test_labels)
-)
+    # Train the model
+    model.fit(
+        train_images, # training data
+        to_categorical(train_labels), # training targets
+        epochs=5,
+        batch_size=32,
+    )
 
-# Save the model weights for later use in model.h5
-model.save_weights('model.h5')
+    # Test the model
+    model.evaluate(
+        test_images,
+        to_categorical(test_labels)
+    )
 
-# Predict on the first 5 test images.
-predictions = model.predict(test_images[:5])
+    # Save the model weights for later use in model.h5
+    model.save_weights('model.h5')
+else:
+    # Load the model's saved weights.
+    model.load_weights('model.h5')
+    
+    # Predict on the first 5 test images.
+    predictions = model.predict(test_images[:5])
 
-# Print our model's predictions.
-print(np.argmax(predictions, axis=1)) # [7, 2, 1, 0, 4]
+    # Print our model's predictions.
+    print(np.argmax(predictions, axis=1)) # [7, 2, 1, 0, 4]
 
-# Check our predictions against the ground truths.
-print(test_labels[:5]) # [7, 2, 1, 0, 4]
+    # Check our predictions against the ground truths.
+    print(test_labels[:5]) # [7, 2, 1, 0, 4]
