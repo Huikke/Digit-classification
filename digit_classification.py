6e68bd570d7e81c63729dd1d6711e9f418a724ff
Lassi Chen <lachen@utu.fi>
2023-06-07 12:46:13 +0300

    add digit_classification run setup, if model doesn't exist

--- digit_classification.py
+++ digit_classification.py
@@ -2,6 +2,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from image_to_vector import image_to_vector
+import os
 
 # Build the model
 model = Sequential([
@@ -9,6 +10,14 @@ model = Sequential([
     Dense(64, activation='relu'),
     Dense(10, activation='softmax', ),
 ])
+
+# Executing setup.py and creates model, if it doesn't exist
+if os.path.exists("model.h5") == False:
+    print("Model doesn't exist, let me create one")
+    with open('setup.py', 'r') as file:
+        setup = file.read()
+    
+    exec(setup)
 
 # Load saved weights into the model.
 model.load_weights('model.h5')
